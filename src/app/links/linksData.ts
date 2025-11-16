0a7e3fcf697951fde450946618d51f97d0aa0709
mabata <mabata@taltech.ee>
2025-11-16 11:16:23 +0100

    Add CV link to linksData for easy access

--- src/app/links/linksData.ts
+++ src/app/links/linksData.ts
@@ -2,6 +2,5 @@ export const LINKS = {
   github: "https://github.com/Estassy",
   linkedin: "https://www.linkedin.com/in/marc-bataba",
   email: "mailto:marcbataba99@gmail.com",
-  cv: "",
+  cv: "/Marc_Bataba_CV_FullStack_Java.pdf",
 };
-
